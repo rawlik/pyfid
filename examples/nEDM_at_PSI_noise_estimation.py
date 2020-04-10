3368f0cae35704ca594fc792f14b2f1223fd93f0
Michał Rawlik <mrawlik@MacBook-MR.local>
2020-04-10 22:42:14 +0200

    Move the noise_from_signal function to filtering.

--- examples/nEDM_at_PSI_noise_estimation.py
+++ examples/nEDM_at_PSI_noise_estimation.py
@@ -13,70 +13,48 @@ np.random.seed(0)
 outdir = os.path.join(os.path.dirname(__file__), "output")
 
 
-def noise_from_signal(T, D, fs, plot=False):
-    """Ref. Sec. 5.1 of Martin Fertl PhD Thesis (2013)."""
-    FFTfreqs = np.fft.fftfreq(T.size, 1 / fs)
-    freqs = FFTfreqs[FFTfreqs >= 0]
-
-    def normedfft(X):
-        return abs(np.fft.fft(X))[FFTfreqs >= 0] * 2 / T.size
-
-    signalFFT = normedfft(D)
-    windowedFFT = normedfft(D * scipy.signal.windows.hann(T.size) / 0.5 * np.sqrt(2/3))
-    filterFFT = pyfid.filtering.filter_frequency_response(
-        freqs=freqs,
-        b=pyfid.nEDMatPSI.filter_b,
-        a=pyfid.nEDMatPSI.filter_a,
-        fs=pyfid.nEDMatPSI.fs)
-
-    mask = (freqs > 7.5) & (freqs < 8.1)
-    mask = mask | (freqs > 12) & (freqs < 13)
-    mask = mask | (freqs > 14) & (freqs < 17)
-    mask = mask | (freqs < 2) | (freqs > 30)
-    mask = ~mask
-
-    def chi2(x):
-        A, C = x
-        return sum((windowedFFT[mask] - (A * filterFFT[mask] + C))**2)
-
-    A, C = scipy.optimize.minimize(chi2, [1.,1.]).x
-
-    noise_amplitude = np.amax(A * filterFFT + C) * fs
-
-    if plot:
-        plt.figure("Noise level from the signal", figsize=(10, 5.5))
-
-        plt.plot(freqs, signalFFT, 'r.-', lw=1.5, ms=4,
-             label='filtered signal power spectrum')
-        plt.plot(freqs, windowedFFT, 'k-', lw=1, ms=4,
-             label='windowed signal power spectrum')
-        plt.plot(freqs[mask], windowedFFT[mask], '.', lw=1.5, ms=4, color="orange",
-             label='windowed signal power spectrum')
-        plt.plot(freqs, A * filterFFT + C, '-', lw=1.5, color="lightgreen",
-             label="filter's frequency response")
-
-        plt.xlabel('frequency (Hz)')
-        plt.xscale('log')
-        plt.yscale('log')
-        plt.xlim(3, 20)
-        # yticks([])
-        plt.grid()
-        plt.legend(loc='upper right', fontsize='medium')
-
-        plt.tight_layout()
-
-    return noise_amplitude
-
-
 T = np.arange(0, 180, 1 / pyfid.nEDMatPSI.fs)
-FFTfreqs = np.fft.fftfreq(T.size, 1 / pyfid.nEDMatPSI.fs)
-freqs = FFTfreqs[FFTfreqs >= 0]
-
 true_noise_amplitude = 10
 N = np.random.randn(T.size) * true_noise_amplitude
 S = 500 * np.exp(-T / 60) * np.sin(2 * np.pi * 7.85 * T)
 
-calculated_noise_amplitude = noise_from_signal(T, pyfid.nEDMatPSI.nEDMfilter(S + N), pyfid.nEDMatPSI.fs, plot=True)
+def mask(f):
+    mask = (f > 7.5) & (f < 8.1)
+    mask = mask | (f > 12) & (f < 13)
+    mask = mask | (f > 14) & (f < 17)
+    mask = mask | (f < 2) | (f > 30)
+
+    return mask
+
+calculated_noise_amplitude, freqs, signalFFT, windowedFFT, m, \
+    fitted_frequency_response = pyfid.filtering.noise_from_signal(
+        D=pyfid.nEDMatPSI.nEDMfilter(S + N),
+        b=pyfid.nEDMatPSI.filter_b,
+        a=pyfid.nEDMatPSI.filter_a,
+        fs=pyfid.nEDMatPSI.fs,
+        mask=mask,
+        full_output=True)
+
+plt.figure("Noise level from the signal", figsize=(10, 5.5))
+
+plt.plot(freqs, signalFFT, 'r.-', lw=1.5, ms=4,
+        label='filtered signal power spectrum')
+plt.plot(freqs, windowedFFT, 'k-', lw=1, ms=4,
+        label='windowed signal power spectrum')
+plt.plot(freqs[m], windowedFFT[m], '.', lw=1.5, ms=4, color="orange",
+        label='windowed signal power spectrum')
+plt.plot(freqs, fitted_frequency_response, '-', lw=1.5, color="lightgreen",
+        label=f"filter's frequency response, max={calculated_noise_amplitude:.2f}")
+
+plt.xlabel('frequency (Hz)')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim(3, 20)
+plt.ylim(1e-6, 1e3)
+plt.grid()
+plt.legend(loc='upper right', fontsize='medium')
+
+plt.tight_layout()
 
 print("True noise amplitude: ", true_noise_amplitude)
 print("Calculated noise amp. ", calculated_noise_amplitude)
--- pyfid/filtering.py
+++ pyfid/filtering.py
@@ -52,3 +52,70 @@ def filter_frequency_response(freqs, b, a, fs):
 
     return abs(h) ** 2
 
+
+def noise_from_signal(D, b, a, fs, mask, full_output=False):
+    """
+    Estimate the amplitude of noise in an FID.
+
+    It is asummed that the signal is white noise that has been filtered.
+    The filter's frequency response is fitted to the spectrum of the signal
+    with selected frequencies masked.
+
+    Parameters
+    ----------
+    D : array
+        The signal to be analysed
+    b, a : arrays
+        The taps of the filter.
+    mask : callable
+        A function to mask the relevant not-noise frequencies in the signal.
+    full_output : bool
+        Whether to return all variables. By default only `noise_amplitude`
+        is returned.
+        sm, A * filterFFT + C
+
+    Returns
+    -------
+    noise_amplitude : float
+        The amplitude of noise.
+    freqs : array
+        The frequencies considered.
+    signalFFT : array
+        The FFT of the signal.
+    windowedFFT : array
+        The signal FFT windowed with a hann window.
+    m : bool array
+        The mask applied to the `freqs` array. Returns True for frequencies
+        to be masked.
+    fitted_frequency_response : array
+        The fitted filter frequency response.
+
+    References
+    ----------
+    .. [1] Sec. 5.1 of Martin Fertl PhD Thesis (2013).
+    """
+    FFTfreqs = np.fft.fftfreq(D.size, 1 / fs)
+    freqs = FFTfreqs[FFTfreqs >= 0]
+
+    def normedfft(X):
+        return abs(np.fft.fft(X))[FFTfreqs >= 0] * 2 / D.size
+
+    signalFFT = normedfft(D)
+    windowedFFT = normedfft(D * scipy.signal.windows.hann(D.size) / 0.5 * np.sqrt(2/3))
+    filterFFT = filter_frequency_response(freqs, b, a, fs)
+
+    m = np.array([ ~mask(f) for f in freqs ])
+
+    def chi2(x):
+        A, C = x
+        return sum((windowedFFT[m] - (A * filterFFT[m] + C))**2)
+
+    A, C = scipy.optimize.minimize(chi2, [1.,1.]).x
+    fitted_frequency_response = A * filterFFT + C
+
+    noise_amplitude = np.amax(fitted_frequency_response) * fs
+
+    if full_output:
+        return noise_amplitude, freqs, signalFFT, windowedFFT, m, fitted_frequency_response
+    else:
+        return noise_amplitude
